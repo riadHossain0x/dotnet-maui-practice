6fc0b8a9101cce2a3593f238ce54f9c33cdb2be4
Riad <riadofficial.me@gmail.com>
2022-12-08 14:08:40 +0600

    data binding

---
+++ DataBinding/MauiAppDemo/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using MauiAppDemo.ViewModels;
+
+namespace MauiAppDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel();
+        }
+
+        private void OnUpdateRateClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                UpdateRateBtn.Text = $"Clicked {count} time";
+            else
+                UpdateRateBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(UpdateRateBtn.Text);
+        }
+    }
+}
---
+++ DataBinding/MauiAppDemo/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MauiAppDemo.ViewModels
+{
+    internal class MainPageViewModel
+    {
+        public decimal UsdRate { get; set; }
+        public decimal EurRate { get; set; }
+        public decimal AudRate { get; set; }
+
+        public ICommand UpdateRateCommand { get; set; }
+
+        public MainPageViewModel()
+        {
+            UpdateRateCommand = new Command(() => OnUpdateRateClicked());
+        }
+
+        private void OnUpdateRateClicked()
+        {
+            UsdRate = 12;
+            EurRate = 16;
+            AudRate = 20;
+        }
+    }
+}
