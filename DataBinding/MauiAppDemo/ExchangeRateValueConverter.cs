4c387a740060c549c1c15c8c63157d4fa82885d2
Riad <riadofficial.me@gmail.com>
2022-12-08 14:35:05 +0600

    data binding

---
+++ DataBinding/MauiAppDemo/ExchangeRateValueConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppDemo
+{
+    internal class ExchangeRateValueConverter : IValueConverter
+    {
+        public string CurrencySymbol { get; set; }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var exchangeRate = decimal.Parse(value.ToString());
+            return $"{exchangeRate.ToString("0.00")} {CurrencySymbol}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
--- DataBinding/MauiAppDemo/ViewModels/MainPageViewModel.cs
+++ DataBinding/MauiAppDemo/ViewModels/MainPageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,17 +8,29 @@ using System.Windows.Input;
 
 namespace MauiAppDemo.ViewModels
 {
-    internal class MainPageViewModel
+    internal class MainPageViewModel : INotifyPropertyChanged
     {
-        public decimal UsdRate { get; set; }
-        public decimal EurRate { get; set; }
-        public decimal AudRate { get; set; }
+        private decimal _usdRate;
+        public decimal UsdRate { get { return _usdRate; } set { _usdRate = value; OnPropertyChanaged(nameof(UsdRate)); } }
+
+        private decimal _eurRate;
+        public decimal EurRate { get { return _eurRate; } set { _eurRate = value; OnPropertyChanaged(nameof(EurRate)); } }
+
+        private decimal _audRate;
+        public decimal AudRate { get { return _audRate; } set { _audRate = value; OnPropertyChanaged(nameof(AudRate)); } }
 
         public ICommand UpdateRateCommand { get; set; }
 
         public MainPageViewModel()
         {
             UpdateRateCommand = new Command(() => OnUpdateRateClicked());
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanaged(string propertyName)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         private void OnUpdateRateClicked()
